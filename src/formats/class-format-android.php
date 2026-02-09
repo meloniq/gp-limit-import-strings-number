693034dbcea8afa916dd32f888165260ced1bae3
meloniq <meloniq@gmail.com>
2026-02-09 21:21:23 +0100

    add filters to format classes

--- src/formats/class-format-android.php
+++ src/formats/class-format-android.php
@@ -2,7 +2,7 @@
 /**
  * GlotPress Format Android XML class
  *
- * @package GlotCore/LimitImportStringsNumber
+ * @package GlotCore\LimitImportStringsNumber
  */
 
 namespace GlotCore\LimitImportStringsNumber;
@@ -23,6 +23,15 @@ class Format_Android extends GP_Format_Android {
 	 */
 	public function read_originals_from_file( $file_name ) {
 		$entries = parent::read_originals_from_file( $file_name );
+
+		/**
+		 * Filter the entries read from the file.
+		 *
+		 * @param Translations|bool $entries The entries read from the file, or false on failure.
+		 * @param GP_Format         $format  The format object used to read the file.
+		 * @param string            $file_name The name of the uploaded file.
+		 */
+		$entries = apply_filters( 'gp_entries_originals_from_file', $entries, $this, $file_name );
 
 		return $entries;
 	}
--- src/formats/class-format-php.php
+++ src/formats/class-format-php.php
@@ -2,7 +2,7 @@
 /**
  * GlotPress Format PHP class
  *
- * @package GlotCore/LimitImportStringsNumber
+ * @package GlotCore\LimitImportStringsNumber
  */
 
 namespace GlotCore\LimitImportStringsNumber;
@@ -24,6 +24,15 @@ class Format_PHP extends GP_Format_PHP {
 	public function read_originals_from_file( $file_name ) {
 		$entries = parent::read_originals_from_file( $file_name );
 
+		/**
+		 * Filter the entries read from the file.
+		 *
+		 * @param Translations|bool $entries The entries read from the file, or false on failure.
+		 * @param GP_Format         $format  The format object used to read the file.
+		 * @param string            $file_name The name of the uploaded file.
+		 */
+		$entries = apply_filters( 'gp_entries_originals_from_file', $entries, $this, $file_name );
+
 		return $entries;
 	}
 
@@ -37,6 +46,16 @@ class Format_PHP extends GP_Format_PHP {
 	 */
 	public function read_translations_from_file( $file_name, $project = null ) {
 		$entries = parent::read_translations_from_file( $file_name, $project );
+
+		/**
+		 * Filter the entries read from the file.
+		 *
+		 * @param Translations|bool $entries The entries read from the file, or false on failure.
+		 * @param GP_Format         $format  The format object used to read the file.
+		 * @param string            $file_name The name of the uploaded file.
+		 * @param GP_Project|null   $project  The project object to read the translations into, or null if not provided.
+		 */
+		$entries = apply_filters( 'gp_entries_translations_from_file', $entries, $this, $file_name, $project );
 
 		return $entries;
 	}
